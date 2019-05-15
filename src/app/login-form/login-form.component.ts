6c2ebcd0d61e5a426cd36ea3b88bc542768e423f
Lloyd Williams <lloydnexus5x@gmail.com>
2019-05-16 01:48:34 +0200

    implemented login form validation

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -12,47 +12,52 @@ export class LoginFormComponent implements OnInit {
 
   loginForm: FormGroup;
   alert: { type: string, message: string };
+  submitted = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private authService: AuthService
-  ) {
-    this.createForm();
-  }
+  ) {}
 
-  createForm() {
-    this.loginForm = this.formBuilder.group({
-      email     : ['', Validators.required],
-      password  : ['', Validators.required]
-    });
-  }
+  get f() { return this.loginForm.controls; }
 
   // attempt login with current values
   onLoginSubmit() {
-    this.authService.login(this.loginForm.value)
-      .then((resp: string) => {
-        // reset form and display message on success
-        this.loginForm.reset();
-        this.alert = { type: 'success', message: resp };
-        setTimeout(() => {
-          this.alert = undefined;
-          // redirect to timeline
-          this.router.navigate(['/timeline']);
-        }, 1000);
-      })
-      .catch((err) => {
-        // alert for either invalid email/password or http error
-        if (err.error) {
-          this.alert = { type: 'danger', message: err.error };
-        } else {
-          this.alert = { type: 'danger', message: `${err.status}, please try again` };
-        }
+    // display validation errors if any
+    this.submitted = true;
+    // return if invalid
+    if (this.loginForm.invalid) {
+      return;
+    } else {
+      this.authService.login(this.loginForm.value)
+        .then((resp: string) => {
+          // reset form and display message on success
+          this.loginForm.reset();
+          this.alert = { type: 'success', message: resp };
+          setTimeout(() => {
+            this.alert = undefined;
+            // redirect to timeline
+            this.router.navigate(['/timeline']);
+          }, 1000);
+        })
+        .catch((err) => {
+          // alert for either invalid email/password or http error
+          if (err.error) {
+            this.alert = { type: 'danger', message: err.error };
+          } else {
+            this.alert = { type: 'danger', message: `${err.status}, please try again` };
+          }
 
-        setTimeout(() => this.alert = undefined, 5000);
-      });
+          setTimeout(() => this.alert = undefined, 5000);
+        });
+    }
   }
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email     : ['', [Validators.required, Validators.email]],
+      password  : ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
 }
